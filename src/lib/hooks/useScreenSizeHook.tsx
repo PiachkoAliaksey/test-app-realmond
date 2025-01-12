2dbb4e36717382c5601e2ff48caa190d954b1bf5
PiachkoAliaksey <a.pechko@agima.ru>
2025-01-12 16:52:53 +0300

    test-app-realmond | refactor files

--- src/lib/hooks/useScreenSizeHook.tsx
+++ src/lib/hooks/useScreenSizeHook.tsx
@@ -1,10 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 
 const useScreenSizeHook = () => {
     const [screenSize, setScreenSize] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: 0,
+        height: 0,
     });
+
+    useLayoutEffect(() => {
+        window && setScreenSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        })
+    }, [])
 
     useEffect(() => {
         const handleResize = () => {
