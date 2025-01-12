6e0d69bafbe18e82ae790c42924e77fb678a2a60
PiachkoAliaksey <a.pechko@agima.ru>
2025-01-12 20:47:40 +0300

    test-app-realmond-000 | refactor README

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Logo from '../../../public/logo.svg'
+import Logo from '../../../public/logo.svg';
 
 const Header = () => {
     return (
